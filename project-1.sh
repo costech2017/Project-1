6ced41af3337d03483f5d6d666ef3a7626663e24
Cosmas <moon_tech@localhost.localdomain>
2023-09-11 13:14:32 +0100

    This is a Encryption and Decryption Project

---
+++ project-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#This Bash scrip will create a file, Encrypt and Decrypt the file
+echo "Encryption and Decryption of file"
+echo
+echo "Please select an operation:"
+choice="Encrypt Decrypt"
+select option in $choice; do
+       if [ $REPLY = 1 ];
+then
+       echo "You have selected Encryption."
+       read -p "Enter the file name: " file
+
+       gpg -c $file
+       echo "This file has been Encrypted."
+else
+       echo "You have selected Decryption."
+       read -p "Enter the file name: " file2
+
+       gpg -d $file2
+       echo "this file has been Decrypted."
+return
+fi
+done        
