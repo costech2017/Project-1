1fba3aa1a71c8ec0a7b90894040d04fd963bad56
Cosmas <moon_tech@localhost.localdomain>
2023-09-12 02:33:01 +0100

    Updated

--- project-1.sh
+++ project-1.sh
@@ -4,11 +4,14 @@ echo "Encryption and Decryption of file"
 echo
 echo "Please select an operation:"
 choice="Encrypt Decrypt"
+
 select option in $choice; do
+
        if [ $REPLY = 1 ];
 then
        echo "You have selected Encryption."
-       read -p "Enter the file name: " file
+       read -p "Enter the file Name: " file
+       
 
        gpg -c $file
        echo "This file has been Encrypted."
@@ -17,7 +20,8 @@ else
        read -p "Enter the file name: " file2
 
        gpg -d $file2
-       echo "this file has been Decrypted."
-return
+       echo "this file, $name2 has been Decrypted."
+       exit
+
 fi
 done        
